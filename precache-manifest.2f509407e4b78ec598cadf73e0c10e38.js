0381a4eead3655b8dd4b16cd7c38b7de731265c5
ikhwansiskom14 <dewa.siskom@gmail.com>
2020-10-10 18:08:03 +0700

    Updates

---
+++ precache-manifest.2f509407e4b78ec598cadf73e0c10e38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2efd82dc8af0b6e0ee284c9c8b2117b6",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "f2294a226017347d1af3",
+    "url": "/test-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac07108c6687285de94d",
+    "url": "/test-deploy/static/js/2.29519075.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy/static/js/2.29519075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2294a226017347d1af3",
+    "url": "/test-deploy/static/js/main.14541a94.chunk.js"
+  },
+  {
+    "revision": "622c4841bdc007c3a001",
+    "url": "/test-deploy/static/js/runtime-main.d60759d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
